3eefd61f390fb6f2ed08d9f88a10e4f96445e11b
raphouqld <rqouille@gmail.com>
2026-04-01 18:14:06 +0200

    `Refactor Parser to take input String (no Scanner), use StringTokenizer, and keep command list accessors for GUI-based game engine`

--- Parser.java
+++ Parser.java
@@ -1,65 +1,53 @@
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * This class is part of the "World of Zuul" application.
  * "World of Zuul" is a very simple, text based adventure game.
  *
- * This parser reads user input and tries to interpret it as an "Adventure"
- * command. Every time it is called it reads a line from the terminal and
- * tries to interpret the line as a two word command. It returns the command
- * as an object of class Command.
+ * This parser receives a line of user input as a String and tries to
+ *  * interpret it as an adventure command. It returns the command as an
+ *  * object of class Command.
  *
  * The parser has a set of known command words. It checks user input against
  * the known commands, and if the input is not one of the known commands, it
  * returns a command object that is marked as an unknown command.
  *
- * @author  Michael Kolling and David J. Barnes + D.Bureau
- * @version 2008.03.30 + 2013.09.15
+ * @author  Michael Kolling and David J. Barnes + D.Bureau + Raphaël Quillaud
+ * @version 2008.03.30 + 2013.09.15 + 2026.04.01
  */
 public class Parser
 {
     private CommandWords aValidCW;  // (voir la classe CommandWords)
-    private Scanner      aReader;   // permettra de lire les commandes au clavier
 
     /**
      * Constructeur par defaut qui cree les 2 objets prevus pour les attributs
      */
     public Parser() {
         this.aValidCW = new CommandWords();
-        this.aReader  = new Scanner( System.in );
-        // System.in designe le clavier, comme System.out designe l'ecran
     } // Parser()
 
     /**
-     * @return The next command from the user.
+     * @param pInputLine the line entered by the user
+     * @return the corresponding command
      */
-    public Command getCommand() {
-        String vInputLine;    // contiendra toute la ligne tapee
+    public Command getCommand(final String pInputLine) {
         String vWord1 = null;
         String vWord2 = null;
 
-        System.out.print( "> " );  // affiche le prompt (invite de commande)
+        StringTokenizer vTokenizer = new StringTokenizer(pInputLine);
 
-        vInputLine = this.aReader.nextLine(); // lit la ligne tapee au clavier
+        if (vTokenizer.hasMoreTokens()) {
+            vWord1 = vTokenizer.nextToken();
+        }
+        if (vTokenizer.hasMoreTokens()) {
+            vWord2 = vTokenizer.nextToken();
+        }
 
-        // cherche jusqu'a 2 mots dans la ligne tapee
-        Scanner vTokenizer = new Scanner( vInputLine );
-        if ( vTokenizer.hasNext() ) {     // y a-t-il un mot suivant ?
-            vWord1 = vTokenizer.next();      // recupere le premier mot
-            if ( vTokenizer.hasNext() ) { // y a-t-il encore un mot suivant ?
-                vWord2 = vTokenizer.next();  // recupere le deuxieme mot
-                // note : on ignore tout le reste de la ligne tapee !
-            } // if
-        } // if
-
-        // Verifie si le premier mot est une commande connue.
-        // Si oui, cree un objet Command avec ce mot. (vWord2 peut etre null)
-        // Sinon, cree une commande vide avec "null" (pour dire 'commande inconnue').
-        if ( this.aValidCW.isCommand( vWord1 ) ) {
-            return new Command( vWord1, vWord2 );
+        if (this.aValidCW.isCommand(vWord1)) {
+            return new Command(vWord1, vWord2);
         }
         else {
-            return new Command( null, null );
+            return new Command(null, vWord2);
         }
     } // getCommand()
 
